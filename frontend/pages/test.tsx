1bacb5ee677fd6364ebc997f3c359c9b622aba43
Edwin <jessicawu414@gmail.com>
2021-10-25 15:38:43 -0400

    Tailwind, also some mucking about

---
+++ frontend/pages/test.tsx
@@ -0,0 +1,11 @@
+import Head from 'next/head'
+export default function TestPage() {
+	return (
+        <div>
+            <Head>
+                <title>Test Page</title>
+            </Head>
+            <h1>This is a Test</h1>
+        </div>
+    )
+}
