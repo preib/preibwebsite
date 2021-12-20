2d06832f1b33e9e36c7cc2eb0b24764518f197d8
Edwin <jessicawu414@gmail.com>
2021-12-20 12:37:17 -0500

    Some page animations

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,14 +1,33 @@
 import '/styles/globals.css'
 import 'tailwindcss/tailwind.css'
 import Layout from '../components/Layout'
+import {motion, AnimatePresence} from "framer-motion"
 
 import '/styles/globalCSS.css'
 
-function MyApp({ Component, pageProps }) {
+function MyApp({ Component, pageProps, router }) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <AnimatePresence>
+      <motion.div key={router.route} initial="pageInitial" animate="pageAnimate" exit="pageExit" variants={
+        {
+          pageInitial: {
+            opacity: 0,
+          },
+          pageAnimate: {
+            opacity: 1,
+            transition: { duration: 0.8 }
+          },
+          pageExit: {
+            opacity: 0,
+            transition: { duration: 0.4 }
+          }
+        }
+      }>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </motion.div>
+    </AnimatePresence>
   )
 }
 
