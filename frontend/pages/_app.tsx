2088457096a42c5859c5692f2fd891d4bcced939
Edwin <jessicawu414@gmail.com>
2021-12-25 11:07:41 -0500

    Global CSS Refactor

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -3,32 +3,30 @@ import 'tailwindcss/tailwind.css'
 import Layout from '../components/global/Layout'
 import {motion, AnimatePresence} from "framer-motion"
 
-import '/styles/globalCSS.css'
-
 function MyApp({ Component, pageProps, router }) {
-  return (
-    <AnimatePresence>
-      <motion.div key={router.route} initial="pageInitial" animate="pageAnimate" exit="pageExit" variants={
-        {
-          pageInitial: {
-            opacity: 0,
-          },
-          pageAnimate: {
-            opacity: 1,
-            transition: { duration: 1.5, ease: 'easeInOut'}
-          },
-          pageExit: {
-            opacity: 0,
-            transition: { duration: 0.5, ease: 'easeInOut'}
-          }
-        }
-      }>
-        <Layout>
-          <Component {...pageProps} />
-        </Layout>
-      </motion.div>
-    </AnimatePresence>
-  )
+	return (
+		<AnimatePresence>
+			<motion.div key={router.route} initial="pageInitial" animate="pageAnimate" exit="pageExit" variants={{
+					pageInitial: {
+						opacity: 0,
+					},
+					pageAnimate: {
+						opacity: 1,
+						transition: { duration: 1.5, ease: 'easeInOut'}
+					},
+					pageExit: {
+						opacity: 0,
+						transition: { duration: 0.5, ease: 'easeInOut'}
+					}
+				}
+			}>
+				{/* Add navbar and footer */}
+				<Layout>
+					<Component {...pageProps} />
+				</Layout>
+			</motion.div>
+		</AnimatePresence>
+	)
 }
 
 export default MyApp
