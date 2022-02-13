589fce2fa0de439507e0167c47bcde6325f4858f
Nathan <nathancai1031@gmail.com>
2022-02-13 13:57:01 -0500

    added courses list in mentor preview and back end to send message

--- frontend/components/mentors/MentorPreview.tsx
+++ frontend/components/mentors/MentorPreview.tsx
@@ -4,21 +4,49 @@ import styles from "/styles/mentorPreview.module.scss";
 
 const MentorPreview = ({ mentor }: { mentor: mentorType }) => {
     console.log(mentor);
-    const backgroundImg = `/background_img/background_${Math.floor(
-        Math.random() * 2 + 1
-    )}.jpg`;
     const chips = [mentor.country, ...mentor.languages];
     const [firstName, setFirstName] = useState("");
     const [lastName, setLastName] = useState("");
     const [country, setCountry] = useState("");
     const [email, setEmail] = useState("");
     const [message, setMessage] = useState("");
+    const [success, setSuccess] = useState(null);
+
+    const submitEmail = async (
+        firstName,
+        lastName,
+        country,
+        email,
+        message
+    ) => {
+        const res = await fetch("/api/contact/mentor", {
+            method: "POST",
+            headers: {
+                Accept: "application/json",
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                firstName,
+                lastName,
+                country,
+                email,
+                message,
+                mentor_name: mentor.firstName,
+                mentor_uuid: mentor.id,
+            }),
+        });
+
+        if (res.status !== 200) setSuccess(false);
+        else setSuccess(true);
+    };
 
     return (
         <div className={styles["mentor-preview__background"]}>
             <img
                 alt="background photo"
-                src={backgroundImg}
+                src={`/background_img/background_${Math.floor(
+                    Math.random() * 2 + 1
+                )}.jpg`}
                 className={styles["mentor-preview__bg-img"]}
             />
             <div>
@@ -52,15 +80,47 @@ const MentorPreview = ({ mentor }: { mentor: mentorType }) => {
                             </div>
                         </div>
                     </div>
+                    <div className={styles["mentor-preview__course-list"]}>
+                        {mentor.courses.map((course, i) => (
+                            <div
+                                key={i}
+                                className={
+                                    styles[
+                                        `mentor-preview__course--${
+                                            course.strength > 50
+                                                ? "strong"
+                                                : "weak"
+                                        }`
+                                    ]
+                                }
+                            >
+                                {course.courseName}
+                            </div>
+                        ))}
+                    </div>
+
                     <div className={styles["mentor-preview__desc"]}>
                         {mentor.description}
                     </div>
-                    <div className={styles["mentor-preview__form"]}>
+                    <form
+                        className={styles["mentor-preview__form"]}
+                        onSubmit={() =>
+                            submitEmail(
+                                firstName,
+                                lastName,
+                                country,
+                                email,
+                                message
+                            )
+                        }
+                    >
                         <h3 className={styles["mentor-preview__title"]}>
                             Want {mentor.firstName} .{mentor.lastName[0]} as a
                             mentor?
                         </h3>
-                        <div className={styles["mentor-preview__fields--format"]}>
+                        <div
+                            className={styles["mentor-preview__fields--format"]}
+                        >
                             <input
                                 className={
                                     styles["mentor-preview__input--small"]
@@ -102,8 +162,12 @@ const MentorPreview = ({ mentor }: { mentor: mentorType }) => {
                             onChange={(e) => setMessage(e.target.value)}
                             placeholder="Your message (subject, goal with mentorship)"
                         />
-                        <button className={styles["mentor-preview__send"]}>Send Message!</button>
-                    </div>
+                        <button className={styles["mentor-preview__send"]}>
+                            Send Message!
+                        </button>
+                    </form>
+                    {success !== null &&
+                        (success ? "Message sent!" : "Failed to sent message!")}
                 </div>
             </div>
         </div>
