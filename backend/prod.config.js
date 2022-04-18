5d6724eeae6cdd5f83860d92892f389a372d887a
Nathan <nathancai1031@gmail.com>
2022-04-18 12:37:37 -0400

    added fallback password

--- backend/prod.config.js
+++ backend/prod.config.js
@@ -4,7 +4,7 @@ console.log(process.env.SQL_PASS);
 module.exports.mysqlConfig = {
     host: process.env.SQL_HOST,
     user: process.env.SQL_USER,
-    password: process.env.SQL_PASS,
+    password: process.env.SQL_PASS || "",
     database: process.env.SQL_DB,
     port: 3306
 };
