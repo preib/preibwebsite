96313d88d60ac6c3535de6b7e0060cb2c51f7cb2
Nathan <nathancai1031@gmail.com>
2022-04-18 12:47:38 -0400

    deleted console logging after bug found

--- backend/controllers/email.js
+++ backend/controllers/email.js
@@ -2,7 +2,6 @@ const config = require('../config');
 const nodemailer = require('nodemailer');
 const { resWriteFail, resWriteSuccess } = require('./response');
 
-console.log(config.password);
 const transporter = nodemailer.createTransport({
     service: 'gmail',
     auth: {
